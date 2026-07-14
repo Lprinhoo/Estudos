0266f6a786b626c6d35197145161e0c01c6b0aa3
danie <danielcostasilva06@gmail.com>
2026-07-14 13:14:02 -0300

    Sad

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,32 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 void main() {
 
+    double xA, xB, xC, yA, yB, yC;
+
+    Locale.setDefault(Locale.US);
     Scanner sc = new Scanner(System.in);
+    System.out.println("Digite os valores do triangulo X");
+    xA = sc.nextDouble();
+    xB = sc.nextDouble();
+    xC = sc.nextDouble();
+
+    System.out.println("Digite os valores do triangulo Y");
+    yA = sc.nextDouble();
+    yB = sc.nextDouble();
+    yC = sc.nextDouble();
+
+    System.out.printf("Aréa do triangulo X: %.4f%", calcularArea(xA, xB, xC));
+    System.out.printf("Aréa do triangulo Y: %.4f%", calcularArea(yA, yB, yC));
 
 }
 
-public static double area(double x, double y, double z) {
+public static double calcularArea(double x, double y, double z) {
 
     double p = (x + y + z) / 2;
-    double area = 0;
+    double area = Math.sqrt(p * (p-x) * (p-y) * (p-z));
 
     return area;
-
 }
 
