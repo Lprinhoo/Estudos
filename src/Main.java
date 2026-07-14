b28a95cd2e851fd22434b6a3a8a0784032bf3d48
danie <danielcostasilva06@gmail.com>
2026-07-14 11:50:40 -0300

    Sad

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+void main() {
+
+    Scanner sc = new Scanner(System.in);
+
+}
+
+public static double area(double x, double y, double z) {
+
+    double p = (x + y + z) / 2;
+    double area = 0;
+
+    return area;
+
+}
+
