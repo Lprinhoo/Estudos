dac4fead8fb3ad18cab9ef1edd7cc715b99d13a3
danie <danielcostasilva06@gmail.com>
2026-07-15 13:28:24 -0300

    Sad

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,5 +2,18 @@ package entities;
 
 public class Product {
 
+    public int     quantity;
+    public double  price;
+    public String  name;
+
+    public double totalValueInStock() { return quantity * price; }
+
+    public void   addProduct() { quantity += 1; }
+
+    public void   removeProduct() {
+
+        if(quantity != 0 ) quantity -= 1;
+        else System.out.println("Sistema não pode remover um produto zerado");;
+    }
 
 }
