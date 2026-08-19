37d67e6c6e0812e05ba347dcc018007d59a5e77b
Level 2D <danielcostasilva06@gmail.com>
2026-08-19 20:02:44 -0300

    Sad

---
+++ src/entities/Product.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Product {
+
+    public int     quantity;
+    public double  price;
+    public String  name;
+
+    public double totalValueInStock() { return quantity * price; }
+
+    public void   addProduct(int quantity) { this.quantity += quantity; }
+
+    public void   removeProduct(int quantity) {
+        if(this.quantity != 0 ) this.quantity -= quantity;
+        else System.out.println("Sistema não pode remover um produto zerado");;
+    }
+
+    public String toString() {
+        return "Dados Atualizados: "+ name + ", R$" + String.format("%.2f", price)
+                + ", "  + quantity + " Unidades, Total: R$" + String.format("%.2f", totalValueInStock());
+    }
+}
