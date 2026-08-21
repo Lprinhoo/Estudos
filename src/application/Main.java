bf73da28a0d19bb4c1df283b515132ebcba8f10f
Level 2D <danielcostasilva06@gmail.com>
2026-08-21 17:54:45 -0300

    Sad

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 import entities.User;
 import entities.Bank;
-import entities.SystemBank;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //chama cadastro de usuario
         User user = User.cadastrarConta(sc);
 
-        System.out.println(Bank.getUsersID());
-
-        SystemBank.mainMenu(user, sc);
+        //inicia sistema do banco
+        Bank.mainMenu(user, sc);
     }
 }
+
